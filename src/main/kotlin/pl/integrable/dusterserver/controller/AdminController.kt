daf20ec55ea436f657b968821d6ce2f540cb1493
Kamil Szewc <kamil.szewc@gmail.com>
2021-01-23 22:25:01 +0100

    Added content editor

--- src/main/kotlin/pl/integrable/dusterserver/controller/AdminController.kt
+++ src/main/kotlin/pl/integrable/dusterserver/controller/AdminController.kt
@@ -1,13 +1,26 @@
 package pl.integrable.dusterserver.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import pl.integrable.dusterserver.repository.ContentRepository
 
 @Controller
 class AdminController {
 
+    @Autowired
+    lateinit var contentRepository: ContentRepository
+
     @GetMapping("/admin")
-    fun admin() : String {
+    fun admin(model: Model) : String {
+
+        val allContent = contentRepository.findAll()
+        model.addAttribute("allContent", allContent)
+
         return "admin"
     }
+
+
 }
--- src/main/kotlin/pl/integrable/dusterserver/controller/ContentController.kt
+++ src/main/kotlin/pl/integrable/dusterserver/controller/ContentController.kt
@@ -1,11 +1,16 @@
 package pl.integrable.dusterserver.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import pl.integrable.dusterserver.model.Content
 import pl.integrable.dusterserver.repository.ContentRepository
+import java.awt.PageAttributes
 
 @Controller
 class ContentController {
@@ -14,10 +19,10 @@ class ContentController {
     lateinit var contentRepository: ContentRepository
 
     @GetMapping("/content")
-    fun information(@RequestParam(name = "name", required = true) name: String,
+    fun information(@RequestParam(name = "id", required = true) id: Long,
                     model: Model) : String {
 
-        val content = contentRepository.findByName(name)
+        val content = contentRepository.findById(id)
         val allContent = contentRepository.findAll();
 
         if (content.isPresent) {
@@ -25,6 +30,37 @@ class ContentController {
             model.addAttribute("allContent", allContent)
 
             return "content"
+        }
+
+        return "redirect:/error"
+    }
+
+    @GetMapping("/content/edit")
+    fun contentEdit(@RequestParam(name = "id", required = false) id: Long,
+                    model: Model) : String {
+
+        val allContent = contentRepository.findAll()
+        val content = contentRepository.findById(id)
+
+        if (content.isPresent) {
+            model.addAttribute("allContent", allContent)
+            model.addAttribute("content", content.get())
+        }
+
+        return "contentEdit"
+    }
+
+    @PostMapping("/content/edit")
+    fun contentEditPost(@RequestParam(name = "id", required = false) id: Long,
+                        content: String) : String {
+
+        val contentObject = contentRepository.findById(id)
+        if (contentObject.isPresent) {
+            contentObject.get().content = content
+
+            contentRepository.save(contentObject.get())
+
+            return "redirect:/content/edit?id=" + id
         }
 
         return "redirect:/error"
